7bce851de1853ade170139ec27111aed2b04d921
Bhavyanth Kondapalli <bhavyanth.kondapalli@partner.bmwgroup.com>
2021-03-21 01:25:59 +0530

    Committing board and square

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Board from './components/Board';
+
+const App = () => {
+  return (
+    <div>
+      <h1>TIC TAC TOE</h1>
+      <Board />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Square.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Square = ({value}) =>{
+    //Setting explicitly button type
+    return <button type="button">{ value }</button>;
+};
+
+export default Square;
